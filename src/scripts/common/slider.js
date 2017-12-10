a47d1cc96805e6475fce75d402d680f7f23707a1
dburdyga <dariaburdyga@gmail.com>
2017-12-11 00:06:07 +0300

    0 06

--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -5,14 +5,14 @@ const controls = document.querySelectorAll('.controls');
 
 let currentSlide = 0;
 
-// осуществляет переход к слайду номер n (начиная с 0)
+
 function goToSlide(n){
     slides[currentSlide].className = 'slide';
-    currentSlide = (n+slides.length)%slides.length; // остаток от деления
+    currentSlide = (n+slides.length)%slides.length;
     slides[currentSlide].className = 'slide showing';
 }
 
-// навешивает обработчики событий на элементы next и previous
+
 function setupListners(){
     next.onclick = function(){
         goToSlide(currentSlide+1);
@@ -22,16 +22,16 @@ function setupListners(){
     }
 }
 
-// показывает кнопки для навигации
+
 function showButtons(){
     for(var i=0; i<controls.length; i++){
         controls[i].style.display = 'inline-block';
     }
 }
 
-// инициализация слайдера
+
 function sliderInit(){
-    if (slides.length !== 0){ // если на странице есть нужный html код
+    if (slides.length !== 0){ //
         setupListners();
         showButtons();
     }

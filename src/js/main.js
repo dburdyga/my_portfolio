7f0250dbcf1d0a1f36e9498dce9dffaaad3f18c6
dburdyga <dariaburdyga@gmail.com>
2017-12-26 16:57:39 +0300

    parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
 let name = 'User Name';
-let place = 'Flavortown';
+let place = 'London';
 
 console.log( `Hello ${name}, ready for ${place}?` );
+
+var parallax = (function () {
+  var bg = document.querySelector('.blog');
+  var user = document.querySelector('.user-blog');
+  var arrow = document.querySelector('.arrow-scroll');
+
+
+
+    return {
+      move: function (block, windowScroll, strafeAmount){
+          var strafe = windowScroll / -strafeAmount + '%';
+          var transformString = 'translate3d(0,'+ strafe +',0)';
+
+          var style = block.style;
+          
+          style.transform= transformString;
+          style.webkitTransform = transformString;
+      },
+      
+      init: function (wScroll) {
+          this.move(bg, wScroll, 45);
+          this.move(user, wScroll, 20);
+          this.move(arrow , wScroll, 20);
+      }
+
+  }
+  
+}());
+
+window.onscroll = function () {
+    var wScroll = window.pageYOffset;
+
+    parallax.init(wScroll);
+}

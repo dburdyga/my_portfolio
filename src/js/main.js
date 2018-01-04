7d3225db9b6f34e84153f08f3309bba44daa2610
dburdyga <dariaburdyga@gmail.com>
2018-01-04 20:32:05 +0300

    alt

--- src/js/main.js
+++ src/js/main.js
@@ -45,3 +45,38 @@ $(document).ready(function(){
 
     });
 });
+
+
+
+
+
+// var parallax = (function () {
+//     var bg = document.querySelector('.welcome_welcome');
+//     var user = document.querySelector('.user');
+//
+//
+//     return {
+//         move: function (block, windowScroll, strafeAmount){
+//             var strafe = windowScroll / -strafeAmount + '%';
+//             var transformString = 'translate3d(0,'+ strafe +',0)';
+//
+//             var style = block.style;
+//
+//             style.transform= transformString;
+//             style.webkitTransform = transformString;
+//         },
+//
+//         init: function (wScroll) {
+//             this.move(bg, wScroll, 45);
+//             this.move(user, wScroll, 20);
+//
+//         }
+//     }
+//
+// }());
+//
+// window.onscroll = function () {
+//     var wScroll = window.pageYOffset;
+//
+//     parallax.init(wScroll);
+// }

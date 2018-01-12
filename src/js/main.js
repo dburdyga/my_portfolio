ade8916437cff37f0a1d6c428fa956f42fcbe5d9
dburdyga <dariaburdyga@gmail.com>
2018-01-12 14:11:21 +0300

    markup9

--- src/js/main.js
+++ src/js/main.js
@@ -56,30 +56,30 @@ window.onscroll = function () {
 
 
 
-var parallaxContainer = document.getElementById('parallax'),
-    layers = parallaxContainer.children;
-
-var moveLayers = function (e) {
-    var initialX = (window.innerWidth / 2) - e.pageX;
-    var initialY = (window.innerHeight / 2) - e.pageY;
-
-    [].slice.call(layers).forEach(function(layer, index) {
-        var
-            divider = index / 100,
-            positionX = initialX * divider,
-            positionY = initialY * divider,
-            bottomPosition = (window.innerHeight / 2) * divider,
-            transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
-            image = layer.firstElementChild;
-
-        layer.style.transform = transformString;
-        image.style.bottom = '-' + bottomPosition + 'px';
-    });
-
-};
-
-
-window.addEventListener('mousemove', moveLayers);
+// var parallaxContainer = document.getElementById('parallax'),
+//     layers = parallaxContainer.children;
+//
+// var moveLayers = function (e) {
+//     var initialX = (window.innerWidth / 2) - e.pageX;
+//     var initialY = (window.innerHeight / 2) - e.pageY;
+//
+//     [].slice.call(layers).forEach(function(layer, index) {
+//         var
+//             divider = index / 100,
+//             positionX = initialX * divider,
+//             positionY = initialY * divider,
+//             bottomPosition = (window.innerHeight / 2) * divider,
+//             transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
+//             image = layer.firstElementChild;
+//
+//         layer.style.transform = transformString;
+//         image.style.bottom = '-' + bottomPosition + 'px';
+//     });
+//
+// };
+//
+//
+// window.addEventListener('mousemove', moveLayers);
 
 
 // var parallax = (function () {
@@ -348,7 +348,7 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: {lat: 51.697354, lng: 39.269249},
         map: map,
-        title: 'Евгений',
+        title: 'Дарья',
         icon: '../images/icons/map_marker.svg'
     });
 

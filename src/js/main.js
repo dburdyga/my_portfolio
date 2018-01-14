41d5a0c53acd23f48844904cc66d19ce6868e65d
dburdyga <dariaburdyga@gmail.com>
2018-01-14 23:23:33 +0300

    changes 3

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,196 @@ let name = 'User Name';
 let place = 'London';
 
 console.log( `Hello ${name}, ready for ${place}?` );
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 51.690372, lng: 39.252002},
+        zoom: 14,
+        styles:[
+            {
+                "featureType": "administrative.country",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#737373"
+                    },
+                    {
+                        "weight": "0.01"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "97"
+                    },
+                    {
+                        "color": "#ffffff"
+                    },
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "lightness": "81"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.landcover",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "100"
+                    },
+                    {
+                        "lightness": "100"
+                    },
+                    {
+                        "gamma": "10.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "100"
+                    },
+                    {
+                        "lightness": "100"
+                    },
+                    {
+                        "gamma": "10.00"
+                    },
+                    {
+                        "weight": "0.01"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.attraction",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.government",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.medical",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "100"
+                    },
+                    {
+                        "lightness": "100"
+                    },
+                    {
+                        "gamma": "10.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.place_of_worship",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.school",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.sports_complex",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#565656"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": "-70"
+                    },
+                    {
+                        "lightness": "43"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#39d2ca"
+                    }
+                ]
+            }
+        ]
+    });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 51.697354, lng: 39.269249},
+        map: map,
+        title: 'Дарья',
+        icon: '../images/icons/map_marker.svg'
+    });
+
+};
+
+
 $(document).ready(function(){
     var firstClick=true;
     console.log(firstClick)
@@ -23,6 +213,25 @@ $(document).ready(function(){
         }
     });
 });
+
+var $item = document.getElementsByClassName('saidbar__item'),
+    activeItem = 'saidbar__item-active';
+
+
+for (var i = 0; i < $item.length; i++) {
+    $item[i].addEventListener('click', function() {
+        if(!(this.classList.contains(activeItem))) {
+            for (var j = 0; j < $item.length; j++) {
+                $item[j].classList.remove(activeItem);
+                this.classList.add(activeItem);
+            }
+        } else {
+            this.classList.remove(activeItem);
+        }
+    })
+};
+
+
 
 var parallax = (function () {
   var bg = document.querySelector('.blog');
@@ -393,22 +602,6 @@ $(document).ready(function(){
 
 
 
-var $item = document.getElementsByClassName('saidbar__item'),
-    activeItem = 'saidbar__item-active';
-
-
-for (var i = 0; i < $item.length; i++) {
-    $item[i].addEventListener('click', function() {
-        if(!(this.classList.contains(activeItem))) {
-            for (var j = 0; j < $item.length; j++) {
-                $item[j].classList.remove(activeItem);
-                this.classList.add(activeItem);
-            }
-        } else {
-            this.classList.remove(activeItem);
-        }
-    })
-};
 
 
 var parallaxContainer = document.getElementById('parallax');
